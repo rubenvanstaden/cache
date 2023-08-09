d0e51d570bf2f254093a432d11ba8e3021c79900
Ruben van Staden <rvs.cipher@proton.me>
2023-08-09 13:45:59 -0400

    update get method with a response channel

--- main.go
+++ main.go
@@ -12,10 +12,15 @@ import (
 // A Memo instance holds the function `f` to memoize, of type `Func`,
 // and the cache, which is a mapping from strings to results.
 
+type entry struct {
+	res   result
+	ready chan struct{} // closed when res is ready
+}
+
 type Memo struct {
 	f     Func
 	mu    sync.Mutex // guards cache
-	cache map[string]result
+	cache map[string]*entry
 }
 
 type Func func(key string) (interface{}, error)
@@ -26,18 +31,33 @@ type result struct {
 }
 
 func New(f Func) *Memo {
-	return &Memo{f: f, cache: make(map[string]result)}
+	return &Memo{
+		f:     f,
+		cache: make(map[string]*entry),
+	}
 }
 
-func (memo *Memo) Get(key string) (interface{}, error) {
-	memo.mu.Lock()
-	res, ok := memo.cache[key]
-	if !ok {
-		res.value, res.err = memo.f(key)
-		memo.cache[key] = res
+func (s *Memo) Get(key string) (interface{}, error) {
+	s.mu.Lock()
+	e := s.cache[key]
+	if e == nil {
+		// This is the first request for this key.
+		// This goroutine becomes responsible for computing
+		// the value and broadcasting the ready condition.
+		e = &entry{ready: make(chan struct{})}
+		s.cache[key] = e
+		s.mu.Unlock()
+
+		e.res.value, e.res.err = s.f(key)
+
+		close(e.ready) // broadcast ready condition
+	} else {
+		// This is a repeat request for this key.
+		s.mu.Unlock()
+
+		<-e.ready // wait for ready condition
 	}
-	memo.mu.Unlock()
-	return res.value, res.err
+	return e.res.value, e.res.err
 }
 
 func httpGetBody(url string) (interface{}, error) {
@@ -49,15 +69,24 @@ func httpGetBody(url string) (interface{}, error) {
 	return ioutil.ReadAll(resp.Body)
 }
 
-var HTTPGetBody = httpGetBody
-
 func incomingURLs() <-chan string {
 	ch := make(chan string)
 	go func() {
 		for _, url := range []string{
 			"https://golang.org",
+			"https://golang.org",
+			"https://golang.org",
+			"https://golang.org",
+			"https://golang.org",
+			"https://godoc.org",
+			"https://godoc.org",
+			"https://godoc.org",
+			"https://godoc.org",
 			"https://godoc.org",
 			"https://play.golang.org",
+			"http://gopl.io",
+			"http://gopl.io",
+			"http://gopl.io",
 			"http://gopl.io",
 			"https://golang.org",
 			"https://godoc.org",
@@ -76,7 +105,6 @@ type M interface {
 }
 
 func Sequential(m M) {
-	//!+seq
 	for url := range incomingURLs() {
 		start := time.Now()
 		value, err := m.Get(url)
@@ -89,7 +117,6 @@ func Sequential(m M) {
 }
 
 func Concurrent(m M) {
-	//!+conc
 	var n sync.WaitGroup
 	for url := range incomingURLs() {
 		n.Add(1)
@@ -105,5 +132,4 @@ func Concurrent(m M) {
 		}(url)
 	}
 	n.Wait()
-	//!-conc
 }
